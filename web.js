103556ff4f74d926c7cfcb82eb22cbe6e587d3b5
Ubuntu <ubuntu@ip-172-31-18-167.us-west-2.compute.internal>
2013-07-09 11:57:11 +0000

    Added web

--- web.js
+++ web.js
@@ -19,3 +19,13 @@ app.get('/', function(request, response) {
 response.send(buffer);
 
 });
+
+
+
+var port = process.env.PORT || 5000;
+
+app.listen(port, function() {
+
+console.log("Listening on " + port);
+
+});
